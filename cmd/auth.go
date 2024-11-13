588306ec8aa7e47daa11cd555aecb09f96e0226c
aidancorcoran <ajcorcoran2001@gmail.com>
2024-11-12 22:01:40 -0500

    auth command WIP

--- cmd/auth.go
+++ cmd/auth.go
@@ -7,10 +7,15 @@ package cmd
 import (
 	"errors"
 	"fmt"
+	"log"
 	"os"
 
+	"github.com/aidancorcoran/gdrive/pkg/auth"
 	"github.com/spf13/cobra"
+	"google.golang.org/api/drive/v3"
 )
+
+var drive_service *drive.Service
 
 // Want to have users run gdrive auth in order to authenticate with their drive
 // This command will have to be ran first in order for the other commands to work
@@ -19,11 +24,19 @@ var auth_cmd = &cobra.Command{
 	Short: "Authenticate with your Google Drive Account",
 	Run: func(cmd *cobra.Command, args []string) {
 		if _, err := os.Stat("./token.json"); errors.Is(err, os.ErrNotExist) {
-			fmt.Print("Token.json does not exist so execute auth command")
+			drive_service, err := auth.GetDriveService()
+			if err != nil {
+				log.Fatalf("Unable to retrieve Drive client: %v", err)
+			}
+			fmt.Println("Authentication successful. Drive service initialized.")
 		} else {
 			fmt.Print("Token.json does exist")
 		}
 	},
+}
+
+func ActiveAccount() *drive.Service {
+	return drive_service
 }
 
 func init() {
